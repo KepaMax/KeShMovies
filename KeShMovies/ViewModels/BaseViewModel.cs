cf4760363c7c9f335cc44fdc7c32bc442bab12e7
KenanMuradov <maljerov.1999@gmail.com>
2022-11-15 13:49:11 +0300

    Добавьте файлы проекта.

---
+++ KeShMovies/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace KeShMovies.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
