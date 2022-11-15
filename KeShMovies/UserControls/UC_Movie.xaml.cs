46fed0f74b8eb3b082e2180db9062bc876c49ec1
KenanMuradov <maljerov.1999@gmail.com>
2022-11-15 15:55:39 +0300

    Beta 0.1

---
+++ KeShMovies/UserControls/UC_Movie.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeShMovies.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для UC_Movie.xaml
+    /// </summary>
+    public partial class UC_Movie : UserControl
+    {
+        public UC_Movie()
+        {
+            InitializeComponent();
+        }
+    }
+}
