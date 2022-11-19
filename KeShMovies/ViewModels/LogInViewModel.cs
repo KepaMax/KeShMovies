623ffa0f7f1b795efa3be1657bc1d71661b1f67b
KenanMuradov <maljerov.1999@gmail.com>
2022-11-19 17:16:17 +0300

    Test Update

--- KeShMovies/ViewModels/LogInViewModel.cs
+++ KeShMovies/ViewModels/LogInViewModel.cs
@@ -13,16 +13,12 @@ public class LogInViewModel :BaseViewModel
 {
     private readonly IUserRepository _userRepository;
     private readonly List<User>? _users;
-
-    public string Email { get; set; } = default!;
-    public string Username { get; set; } = default!;
+    public string UsernameOrEmail { get; set; } = default!;
     public string Password { get; set; } = default!;
 
     public LogInViewModel(IUserRepository userRepository)
     {
         _userRepository = userRepository;
-
         _users = _userRepository.GetList() != null ? _userRepository.GetList() : new();
-
     }
 }
